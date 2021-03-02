68828f3ba25d4eb7117ed8218cfe571a085d8ee0
Sheneska Williams <74882676+sheneska@users.noreply.github.com>
2021-03-01 21:53:32 -0500

    Revert "Update the README.md file to describe the repo."

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,24 +13,16 @@
 // limitations under the License.
 
 /**
- * A random greeting
+ * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
   const greetings =
-      ['I love sushi', 'I live by the mantra- turn your obligations into opportunities.'
-        , 'I was named from the author Sheneska Jackson', 'I enjoy helping others!'];
+      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
-  //const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
-}
-
-function gallery(){
-    const galleryContainer = document.getElementById('galleryContainer');
-
-    galleryContainer.style.display = "block";
 }
