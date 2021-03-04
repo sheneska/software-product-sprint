9652feddc64185f406fa538ff8f05f0424a670e9
sheneska <sheneska101@gmail.com>
2021-03-04 02:59:39 +0000

    Create an array list with random words, then applied concepts of parsing JSON and GSON to display it on screen.

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,7 +2,13 @@ async function showServerNewConcepts (){
     const responseFromServer = await fetch('/myportfolio');
     const textFromResponse = await responseFromServer.text();
 
-    const newconceptsContainer = document.getElementById('newconcepts-container');
-    newconceptsContainer.innerText = textFromResponse;
+    const desserts = await responseFromServer.json();
+
+    console.log(desserts.cheesecake);
+    console.log(desserts.cupcake);
+    console.log(desserts.Icecream);
+    
+    /*const newconceptsContainer = document.getElementById('newconcepts-container');
+    newconceptsContainer.innerText = textFromResponse;*/
 }
 
--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import java.util.ArrayList;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/myportfolio")
@@ -12,7 +14,19 @@ public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>I learnt Javascript, HTML and front end/back end web development. </h1>");
+    //response.setContentType("text/html;");
+    //response.getWriter().println("<h1>I learnt Javascript, HTML and front end/back end web development. </h1>");   
+    ArrayList<String> desserts = new ArrayList<String>();
+    desserts.add("Cheesecake");
+    desserts.add("Cupcake");
+    desserts.add("Icecream");
+
+    Gson gson = new Gson();
+    String json = gson.toJson(desserts);
+
+    //ArrayList<String> desserts = new ArrayList<String>();
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
