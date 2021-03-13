d7d6c54698a870b1ecd803f22f230fffe001a28d
sheneska <sheneska101@gmail.com>
2021-03-13 00:10:43 +0000

    Modified code to include a map on the website.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -34,3 +34,9 @@ function gallery(){
 
     galleryContainer.style.display = "block";
 }
+
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
